05115c9f88716c26791ad0cb39d2a5f0e67fad05
FullIdle <2675423979@qq.com>
2024-07-24 21:10:41 +0800

    基本框架完善 添加papi对比条件和命令执行奖品

--- src/main/java/me/gsqfi/fitask/fitask/taskComponent/rewards/CommandReward.java
+++ src/main/java/me/gsqfi/fitask/fitask/taskComponent/rewards/CommandReward.java
@@ -3,6 +3,8 @@ package me.gsqfi.fitask.fitask.taskComponent.rewards;
 import com.google.gson.*;
 import lombok.Getter;
 import lombok.Setter;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
 import java.lang.reflect.Type;
@@ -20,7 +22,26 @@ public class CommandReward implements IReward<CommandReward>{
 
     @Override
     public void giveReward(OfflinePlayer player) {
-
+        String papi = PlaceholderAPI.setPlaceholders(player,command.replace('&', '§'));
+        if (command.startsWith("tell: ")) {
+            player.getPlayer().sendMessage(papi.substring(6));
+            return;
+        }
+        if (command.startsWith("command: ")){
+            Bukkit.dispatchCommand(player.getPlayer(),papi.substring(9));
+            return;
+        }
+        if (command.startsWith("op: ")){
+            boolean op = player.isOp();
+            player.setOp(true);
+            Bukkit.dispatchCommand(player.getPlayer(),papi.substring(4));
+            player.setOp(op);
+            return;
+        }
+        if (command.startsWith("console: ")){
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),papi.substring(9));
+            return;
+        }
     }
 
     @Override
